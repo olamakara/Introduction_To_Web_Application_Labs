50ac564aa39576797a5fe18e3a03fda15fb3e086
olamakara <101846943+olamakara@users.noreply.github.com>
2023-03-02 00:22:37 +0100

    hope all is here

---
+++ Lab_6/TripList/src/environment/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,  // false?
+    firebaseConfig: {
+    apiKey: "AIzaSyD74iIUlQPUMtmqBRqUoZOTFQzjCPfvhKI",
+    authDomain: "travelwebsite-1069c.firebaseapp.com",
+    projectId: "travelwebsite-1069c",
+    storageBucket: "travelwebsite-1069c.appspot.com",
+    messagingSenderId: "159681626652",
+    appId: "1:159681626652:web:592c73fd1777fa000695d9",
+    measurementId: "G-XR2ZTV7KP3"
+    }
+};
